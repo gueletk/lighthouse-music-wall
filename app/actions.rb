1286e4eed43767103bf9da02576131ccab703eb5
Ksenia <gueletina.ksenia@gmail.com>
2016-04-18 11:23:04 -0400

    user can create a track

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,25 @@
 get '/' do
   erb :index
 end
+
+get '/tracks' do
+  erb :'tracks/index'
+end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+post '/tracks/new' do
+  @track = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'/tracks/new'
+  end
+end
---
+++ db/migrate/20160418124742_create_track_table.rb
@@ -0,0 +1,11 @@
+class CreateTrackTable < ActiveRecord::Migration
+  def change
+    create_table :tracks do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
