83412b4fc1a329a7eab8f1102dac0a1b9bd3df12
Ksenia <gueletina.ksenia@gmail.com>
2016-04-18 17:34:06 -0400

    added nav bar with login/logout button depending on whether the user is logged in. user needs to be logged in to add a track. track displays user.

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,30 @@
 # Homepage (Root path)
 enable :sessions
 
+# helpers do
+#   def protected!
+#     return if authorized?
+#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+#     halt 401, "Not authorized\n"
+#   end
+#
+#   def authorized?
+#     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+#     if @auth.provided? and @auth.basic? and @auth.credentials
+#       username, password = @auth.credentials
+#       User.validate(username, password)
+#     end
+#   end
+# end
+
+helpers do
+
+  def loggedin?
+    session[:user_id]
+  end
+
+end
+
 get '/' do
   erb :index
 end
@@ -11,6 +35,8 @@ get '/tracks' do
 end
 
 get '/tracks/new' do
+  loggedin?
+  #protected!
   @track = Track.new
   erb :'tracks/new'
 end
@@ -19,7 +45,8 @@ post '/tracks/new' do
   @track = Track.new(
     title: params[:title],
     author: params[:author],
-    url: params[:url]
+    url: params[:url],
+    user_id: session[:user_id]
   )
   if @track.save
     redirect '/tracks'
@@ -36,9 +63,37 @@ end
 post '/login' do
   @user = User.find_by username: params[:username]
   if @user.password == params[:password]
-    session[:username] = params[:username]
+    session[:user_id] = @user.id
     erb :'/loggedin'
   else
     redirect '/login'
   end
+end
+
+get '/signup' do
+  @user = User.new(
+  username: params[:username],
+  name: params[:name],
+  email: params[:email]
+  )
+  erb :'/signup'
+end
+
+post '/signup' do
+  @user = User.new(
+  username: params[:username],
+  password: params[:password],
+  name: params[:name],
+  email: params[:email]
+  )
+  if @user.save
+    redirect '/login'
+  else
+    erb :'/signup'
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   has_many :tracks
   has_many :upvotes
 
+  validates :username, :email, uniqueness: true
+  validates :username, :password, :email, :name,
+    presence: true
+
+  # def self.signedin?
+  #   user = find_by username: username
+  #   if user && user.password == password
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
 end
