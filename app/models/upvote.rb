1e2c28e288a31fcc0023688878e50ff271b992e7
Ksenia <gueletina.ksenia@gmail.com>
2016-04-18 18:30:37 -0400

    upvote button only displays if user has not voted on that song

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -3,4 +3,8 @@ class Upvote < ActiveRecord::Base
   belongs_to :user
   belongs_to :track
 
+  def self.exists?(user_id, track_id)
+    find_by(user_id: user_id, track_id: track_id) ? true : false
+  end
+
 end
